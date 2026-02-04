eb4f34324e16f690775f54b325f429c5a6b59153
Enzo Figlioli <enzofiglioli.p@gmail.com>
2026-02-04 11:16:52 -0300

    agregar configuración CORS para permitir solicitudes desde dominios específicos

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: ['https://malipi.vercel.app/', 'http://localhost:5173&'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
